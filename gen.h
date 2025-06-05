4b5f18efea6c3b50d58d7ee06616d4ae1071443f
benbenwu176 <ben@wulabs.com>
2025-06-05 09:49:08 -0500

    dp branch "merge"

--- gen.h
+++ gen.h
@@ -1,6 +1,7 @@
 #ifndef GEN_H
 #define GEN_H
-#include <stdint.h>
+
+#include <vector>
 
 #define C2_MIDI 36 
 #define C4_MIDI 60
@@ -9,29 +10,77 @@
 #define OCTAVE_INTERVAL 12 // MIDI range of an octave
 #define NUM_WHACKER_PITCHES 32 // The number of boomwhackers from G5-C3
 
+enum add_flags {
+    NO_RECURSE = 0, // Disables recursive offloading
+    RECURSE = 1 << 0, // Allows recursive offloading
+    ALLOCATE = 1 << 1, // Allows allocation of new boomwhackers
+    PROXIMATE = 1 << 2, // Allows proximate notes to be played by nearby players
+    ROOT = 1 << 3, // Denotes the root of the recursive offloading tree
+    LAST_RESORT = RECURSE | ALLOCATE | PROXIMATE // Allows all options to be used
+};
+
+typedef struct Config {
+    // Note data
+    int num_notes; // number of notes in the piece
+
+    // Parameter data
+    int num_players; // The number of players in the ensemble
+    int whacker_overflow_limit; // The max # of boomwhackers that can be played at once
+    int whackers_per_pitch; // The number of boomwhackers per pitch
+
+    // Rate data
+    double switch_time; // The time, in seconds, it takes a player to switch boomwhackers
+
+} Config;
+
 // Define the Note struct
 typedef struct Note {
-    double time; // The time, in seconds, the note occurs at in the piece
-    struct Note* next; // Pointer to the next note of the same pitch
-    int id; // Index of the note within the note array
-    int player; // The player that this note is assigned to
-    int pitch; // MIDI pitch value
-    int whackerIndex; // Index of the whacker being used for this note
-    bool capped; // Whether the note is played with a capped boomwhacker
-    bool proximate; // Whether the note is proximate
-    bool conflicting; // Whether the note creates a switch conflict
+    double time;          // The time, in seconds, the note occurs at in the piece
+    int id;               // Index of the note within the note array
+    int player;           // The player that this note is assigned to
+    int pitch;            // MIDI pitch value
+    int whacker_index;     // The index of the boomwhacker that is used for this note
+    bool capped;          // Whether the note is played with a capped boomwhacker
+    bool proximate;       // Whether the note is proximate
+    bool conflicting;     // Whether the note creates a switch conflict
 } Note;
 
 typedef struct Boomwhacker {
+    int pitch; // The pitch of the boomwhacker
     bool used; // Whether the boomwhacker has been used
     bool capped; // Whether the boomwhacker is capped
+    std::vector<Note*> notes; // The notes that are played by this boomwhacker
 } Boomwhacker;
 
+typedef struct Player {
+    int id;
+    std::vector<Boomwhacker*> whackers; // The boomwhackers that the player has
+    std::vector<Note*> notes; // The notes that the player has played
+    std::vector<Note*> bucket; // The player's bucket of notes. Refer to documentation for details.
+    double attempt_add(Note* note, bool force);
+    double force_add(Note* note);
+} Player;
+
+typedef struct Edge{
+    int dst; // The destination node
+    Note* note; // The note that this edge is connected to
+} Edge;
+
+typedef struct Node {
+    int player; // The player this node is assigned to
+    std::vector<Edge> edges; // The edges that are connected to this node
+} Node;
+
+typedef struct Graph {
+    std::vector<Node> nodes; // Array of nodes
+} Graph;
+
 typedef struct Assignment {
-    Note** noteArray; // Array of pointers to notes
-    Note** whackerTable; // Hash table of pointers to note heads
-    Boomwhacker** whackerStatuses; // Array of pointers to boomwhackers
-    int pScore; // Playability score
+    std::vector<Note> notes;
+    std::vector<Boomwhacker*> whackers;
+    std::vector<Player> players;
+    std::vector<std::vector<Note*>> mrp;
+    Graph adjacency_graph;
 } Assignment;
 
 #endif  // GEN_H
---
+++ gen.py
@@ -0,0 +1,182 @@
+import ms3
+import numpy as np
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+import pandas as pd
+import struct
+import io
+import subprocess
+from fractions import Fraction
+from collections import namedtuple
+from datetime import datetime
+
+filename = 'mario'
+score = ms3.Score(f'./data/{filename}.mscz').mscx
+  
+score_bs4 = score.parsed
+
+# red, green, blue, purple, orange, light blue, pink, brown, black, TODO: gray?, light green but like different
+colors = ['#FF0000', '#00AA00', '#0000FF', "#D752FF", '#EE8000', '#00A0FF', '#FF90FF', '#AA5500', '#000000']
+# TODO: Add more colors
+
+notes_df = score.notes()
+chords_df = score.chords()
+measures_df = score.measures()
+
+chords_df['quarterbeats'] = chords_df['quarterbeats'].apply(float)
+chords_df.sort_values(by=['quarterbeats'], inplace=True)
+chords_df['quarterbeats'] = chords_df['quarterbeats'].apply(Fraction)
+
+# IMPORTANT: Maintain this struct format with the corresponding Note struct in gen.h
+Note = namedtuple('Note', ['time', 'id', 'player', 'pitch', 'whacker_index', 'capped', 'proximate', 'conflicting'])
+
+def compute_chord_timings():
+  # Initialize variables for time calculation
+  current_time = Fraction(0) # start at the beginning of the piece
+  current_qpm = 120 # default to 120
+  SECONDS_PER_MINUTE = 60 # seconds per minute do some math
+  WHOLE_NOTE_DURATION = 4 # quarter notes per whole note
+  # Create new fields in the dataframe
+  measures_df['time_start'] = Fraction(0)
+  measures_df['time_end'] = Fraction(0)
+  chords_df['time'] = Fraction(0)
+  # Initialize chord iterator
+  chord_iter = chords_df.iterrows()
+  has_next = True
+  
+  try: # bad code here this makes you try/except N times, N = # of chords
+    i, c = next(chord_iter)
+  except StopIteration:
+    has_next = False
+  # Iterate over measures
+  for index, row in measures_df.iterrows():
+    # Set measure start time
+    if (index == 0): # Beginning of the piece if first measure
+      measures_df.at[index, 'time_start'] = Fraction(0)
+    else: # End time of previous measure if not first
+      measures_df.at[index, 'time_start'] = measures_df.iloc[index - 1]['time_end']
+
+    remaining_duration = Fraction(row['duration_qb'])
+    while c['mc'] == row['mc'] and has_next:
+      if c['event'] == 'Chord': # Calculate time offset from measure start time and update chord times
+        mc_qb_onset = c['mc_onset'] * WHOLE_NOTE_DURATION
+        # current time = measure start time + time offset (normalized to quarter notes)
+        time_offset = Fraction(Fraction(SECONDS_PER_MINUTE / current_qpm) * mc_qb_onset)
+        current_time = measures_df.iloc[index]['time_start'] + time_offset
+        remaining_duration = row['duration_qb'] - mc_qb_onset
+        chords_df.at[i, 'time'] = current_time
+      elif c['event'] == 'Tempo': # update current tempo
+        current_qpm = c['qpm']
+      try: 
+        i, c = next(chord_iter)
+      except StopIteration:
+        has_next = False
+    # set measure end time and update current time
+    measures_df.at[index, 'time_end'] = Fraction(current_time + Fraction(SECONDS_PER_MINUTE / current_qpm) * remaining_duration)
+    current_time = measures_df.iloc[index]['time_end']
+
+def update_note_timings():
+  # Update note timings to match the timing of the chord they belong to
+  notes_df['time'] = Fraction(0) # initialize time field
+  for index, row in notes_df.iterrows():
+    note_chord_id = row['chord_id']
+    chord_time = chords_df.loc[chords_df['chord_id'] == note_chord_id, 'time'].values[0]
+    notes_df.at[index, 'time'] = chord_time
+
+def time_init():
+  compute_chord_timings()
+  update_note_timings()
+
+def print_params(pvalues, note_pitches, note_times, num_elements):
+  print(pvalues)
+  print(note_pitches)
+  print(note_times)
+  for time in note_times:
+    print(time, end = ", ")
+  print(len(pvalues))
+  print(num_elements)
+  
+# Run the genetic algorithm to get assignments
+def gen():
+  params = {
+    'num_players': 9, # number of players in the ensemble
+    'max_whackers': 2, # maximum number of whackers someone can play at once
+    'whackers_per_pitch': 2 # the number of whackers available for each pitch, we are poor so we have 2
+  }
+  rates = {
+    'switch_time': 2.0, # time, in seconds, it takes a player to switch boomwhackers
+  }
+  pvalues = list(params.values())
+  rvalues = np.array(list(rates.values()), dtype = np.float64)
+  note_pitches = notes_df['midi'].values
+  note_times = np.array([float(f) for f in notes_df['time']])
+  n = len(notes_df)
+  p = len(pvalues)
+  r = len(rvalues)
+    
+  # Create a byte stream of # of notes, params, rates then add note pitches and times
+  data_size = n*4 + n*8 + p*4 + r*8
+  padding = 4096 - (data_size) % 4096
+  stream_format = ('i' * n) + ('d' * n) + ('i' * p) + ('d' * r) + ('x' * padding)
+  stream = struct.pack(stream_format, *note_pitches, *note_times, *pvalues, *rvalues)
+  
+  proc = subprocess.run(['./gen.exe', str(n), str(p), str(r)], input = stream, capture_output=True) # Add back check = True
+  print(n)
+  # Get the current date and time
+  current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+  # Write the date and time to the file
+  with open('gen_output.txt', 'w') as f:
+    f.write(f"Date and Time: {current_datetime}\n")
+
+  # Append the stderr contents to the file
+  with open('gen_output.txt', 'ab') as f:
+    f.write(proc.stderr.decode('utf-8').encode())
+    
+  if (proc.returncode != 0):
+    print("Error in subprocess")
+    return
+
+  # IMPORTANT: Maintain this struct format with the corresponding Note struct in gen.h
+  note_struct_format = "diiii???xxxxx" # Double, integer, bool, padding
+  note_struct_size = struct.calcsize(note_struct_format)
+  note_struct_parser = struct.Struct(note_struct_format)
+  notes = []
+  note_data = proc.stdout
+  # Read num of conflicting notes from the first 4 bytes of the output
+  num_conflicting_notes = struct.unpack('i', note_data[:4])[0]
+  # Read flattened array of player indexes from the next 4 * num_players bytes of the output
+  player_indexes = np.frombuffer(note_data[4:4 + 4 * params['num_players']], dtype=np.int32)
+  # Start reading note data from the 4 * num_players + 4 bytes offset
+  byte_offset = 4 + 4 * params['num_players']
+  for offset in range(byte_offset ,len(proc.stdout), note_struct_size):
+    chunk = note_data[offset : offset + note_struct_size]
+    if (len(chunk) < note_struct_size):
+      # End of data, just padding here
+      break
+    # Unpack the data into a tuple of Python values
+    unpacked = note_struct_parser.unpack(chunk)
+    
+    time, id, player, pitch, whacker_index, capped, proximate, conflicting = unpacked
+    notes.append(Note(time, id, player, pitch, whacker_index, capped, proximate, conflicting))
+    
+  recolor(notes)
+
+  
+# Recolor the notes with the generated assignments. IMPORTANT: Notes that are already colored in the original mscz file will NOT be recolored.
+def recolor(notes):
+  # Only relevant in rare edge case of bad writing where whacker notes are written longer than they should be and on top of each other
+  STUPID_PEOPLE_BUF = 0.001
+  for index, row in notes_df.iterrows():
+    score_bs4.color_notes(row['mc'], row['mc_onset'], row['mc'], row['mc_onset'] + STUPID_PEOPLE_BUF,
+                midi=[row['midi']], color_html = colors[notes[index].player])
+    
+  notes_df['time'] = [note.time for note in notes]
+  notes_df['player'] = [note.player for note in notes]
+  notes_df['capped'] = [note.capped for note in notes]
+  score.store_score(f'./data/{filename}.mscx')
+  notes_df.to_csv("notes.csv")
+
+
+time_init()
+gen()
